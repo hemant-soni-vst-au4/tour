866bf7fbddbbfbdd37b2454348c5e50b276e53ce
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-09 21:38:46 +0530

    style add

--- src/Tour.js
+++ src/Tour.js
@@ -7,7 +7,12 @@ function Tour({ id, image, info, price, name }) {
       <footer>
         <div className="tour-info">
           <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
         </div>
+        <p>
+            {info}
+        </p>
+        <button className="delete-btn">not interested</button>
       </footer>
     </article>
   );
