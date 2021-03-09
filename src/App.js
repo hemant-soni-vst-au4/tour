d1507cfb32572826f91279142b9c6b53952c02da
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-09 21:11:17 +0530

    loading componen intialise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Loading from './Loading'
+const url = "https://course-api.com/react-tours-project";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([])
+  if (loading) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Loading />
     </div>
   );
 }
+};
 
 export default App;
