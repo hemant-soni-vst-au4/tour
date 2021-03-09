54e695497aba13e1a494fab43e413e301ebe25fe
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-03-09 21:23:52 +0530

    tour api fetched

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
-import Loading from './Loading'
+import React, { useState, useEffect } from "react";
+import Loading from './Loading';
+import Tours from './Tours'
 const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([])
+  const fetchTours = async () => {
+    setLoading(true)
+    const response = await fetch(url);
+    const tour = await response.json();
+    console.log(tour)
+  }
+  useEffect(() => {
+   fetchTours();
+  }, [])
+
   if (loading) {
   return (
     <div>
@@ -12,6 +23,10 @@ const App = () => {
     </div>
   );
 }
+return ( <main>
+  <Tours />
+</main>
+)
 };
 
 export default App;
